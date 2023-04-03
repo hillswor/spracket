6c94a691e93077fe746beabe6495b74355bf5d88
Bruce <bruce.hillsworth@gmail.com>
2023-04-03 16:07:19 -0600

    Reworked code to get bikes to pull through.  Added fetch to localStolenBikes function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-let zipCode;
-
 const url = `https://ipgeolocation.abstractapi.com/v1/?api_key=${apiKey}`;
 
-function getZipCode(response) {
+function localStolenBikes(response) {
 	locationObject = JSON.parse(response);
-	zipCode = locationObject.postal_code;
+	const zipCode = locationObject.postal_code;
+	fetch(
+		`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&query=image&location=${zipCode}&distance=50&stolenness=proximity`
+	)
+		.then((response) => response.json())
+		.then((bikes) => {
+			console.log(bikes);
+		});
 }
 
 function httpGetAsync(url, callback) {
@@ -17,24 +22,4 @@ function httpGetAsync(url, callback) {
 	xmlHttp.send(null);
 }
 
-httpGetAsync(url, getZipCode);
-
-const card = document.querySelector(".card");
-
-let zipcode = 97239;
-let bikeInfo;
-
-fetch(
-  `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&query=image&location=${zipcode}&distance=50&stolenness=proximity`
-)
-  .then((response) => response.json())
-  .then((bikes) => {
-    console.log(bikes);
-    bikeInfo = bikes;
-    const cardImage = document.createElement("img");
-    cardImage.src = bikeInfo.bikes[0].large_img;
-    card.append(cardImage);
-    // console.log(bikeInfo.bikes);
-    // console.log(bikeInfo.bikes.filter((x) => x.large_img));
-  });
-
+httpGetAsync(url, localStolenBikes);
