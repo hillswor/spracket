bfd49e61a3d1f9acb22723ee577dae3bfd73d031
Dustin <dustinhollowaypdx@gmail.com>
2023-04-04 09:38:10 -0700

    extended filter conditions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-let apiKey = ;
+let apiKey = "34eac09b0f8348b3912237e3325d9bd4";
 const url = `https://ipgeolocation.abstractapi.com/v1/?api_key=${apiKey}`;
 
 function httpGetAsync(url, callback) {
@@ -10,11 +10,11 @@ function httpGetAsync(url, callback) {
   xmlHttp.open("GET", url, true); // true for asynchronous
   xmlHttp.send(null);
 }
+let bikeInfo;
 
 const card = document.querySelector(".card");
 const gallery = document.querySelector(".gallery");
-let bikeInfo;
-
+let distance = 50;
 const renderer = (bikeInfo) => {
   bikeInfo.forEach;
   const card = document.createElement("div");
@@ -35,7 +35,7 @@ const renderer = (bikeInfo) => {
     imageInfo = !imageInfo;
     const p2 = document.createElement("p");
     p2.setAttribute("class", "description");
-    p2.textContent = bikeInfo.description;
+    p2.textContent = bikeInfo.title;
     card.append(p2);
   });
 };
@@ -45,14 +45,28 @@ function getZipCode(response) {
   const zipCode = locationObject.postal_code;
 
   fetch(
-    `https://bikeindex.org:443/api/v3/search?page=1&per_page=6&query=image&location=${zipCode}&distance=50&stolenness=proximity`
+    `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&query=image&location=${zipCode}&distance=${distance}&stolenness=proximity`
   )
     .then((response) => response.json())
     .then((stolenBikes) => {
       //   console.log(stolenBikes);
-      bikeInfo = stolenBikes.bikes.filter((x) => x.large_img);
+      bikeInfo = stolenBikes.bikes.filter(
+        (x) => x.large_img && x.title && x.description
+      );
       console.log(bikeInfo);
-      bikeInfo.forEach(renderer);
+      bikeInfo.length > 25 ? bikeInfo.forEach(renderer) : (distance = 200);
+      fetch(
+        `https://bikeindex.org:443/api/v3/search?page=1&per_page=100&query=image&location=${zipCode}&distance=${distance}&stolenness=proximity`
+      )
+        .then((response) => response.json())
+        .then((stolenBikes) => {
+          //   console.log(stolenBikes);
+          bikeInfo = stolenBikes.bikes.filter(
+            (x) => x.large_img && x.title && x.description
+          );
+          console.log(bikeInfo);
+          bikeInfo.forEach(renderer);
+        });
     });
 }
 
