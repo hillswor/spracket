f947bfdf40858c18c2940b3ef680a72fa0c4cd3e
Dustin <dustinhollowaypdx@gmail.com>
2023-04-03 17:50:16 -0700

    successful fetch with 4 cards

--- src/index.js
+++ src/index.js
@@ -1,40 +1,46 @@
-let zipCode;
-
+let apiKey = ;
 const url = `https://ipgeolocation.abstractapi.com/v1/?api_key=${apiKey}`;
 
-function getZipCode(response) {
-	locationObject = JSON.parse(response);
-	zipCode = locationObject.postal_code;
-}
-
 function httpGetAsync(url, callback) {
-	const xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function () {
-		if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
-			callback(xmlHttp.responseText);
-	};
-	xmlHttp.open("GET", url, true); // true for asynchronous
-	xmlHttp.send(null);
+  const xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+      callback(xmlHttp.responseText);
+  };
+  xmlHttp.open("GET", url, true); // true for asynchronous
+  xmlHttp.send(null);
+}
+const card = document.querySelector(".card");
+const gallery = document.querySelector(".gallery");
+let bikeInfo;
+
+const renderer = (bikeInfo) => {
+  bikeInfo.forEach;
+  const card = document.createElement("div");
+  card.setAttribute("class", "card");
+  const cardImage = document.createElement("img");
+  cardImage.src = bikeInfo.large_img;
+  const p1 = document.createElement("p");
+  p1.setAttribute("id", "bike-name");
+  p1.textContent = bikeInfo.frame_model;
+  card.append(cardImage, p1);
+  gallery.append(card);
+};
+
+function getZipCode(response) {
+  locationObject = JSON.parse(response);
+  const zipCode = locationObject.postal_code;
+
+  fetch(
+    `https://bikeindex.org:443/api/v3/search?page=1&per_page=4&query=image&location=${zipCode}&distance=50&stolenness=proximity`
+  )
+    .then((response) => response.json())
+    .then((stolenBikes) => {
+      //   console.log(stolenBikes);
+      bikeInfo = stolenBikes.bikes.filter((x) => x.large_img);
+      console.log(bikeInfo);
+      bikeInfo.forEach(renderer);
+    });
 }
 
 httpGetAsync(url, getZipCode);
-
-const card = document.querySelector(".card");
-
-let zipcode = 97239;
-let bikeInfo;
-
-fetch(
-  `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&query=image&location=${zipcode}&distance=50&stolenness=proximity`
-)
-  .then((response) => response.json())
-  .then((bikes) => {
-    console.log(bikes);
-    bikeInfo = bikes;
-    const cardImage = document.createElement("img");
-    cardImage.src = bikeInfo.bikes[0].large_img;
-    card.append(cardImage);
-    // console.log(bikeInfo.bikes);
-    // console.log(bikeInfo.bikes.filter((x) => x.large_img));
-  });
-
