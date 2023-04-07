b859c43e4d4efe01377c9fcecaef62078b04d47c
Dustin <dustinhollowaypdx@gmail.com>
2023-04-07 10:24:30 -0700

    updated fetch url

--- src/index.js
+++ src/index.js
@@ -313,7 +313,7 @@ function renderSortedBikes(bike) {
 function initialize(response) {
   if (response === null) {
     fetch(
-      "https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=IP&distance=10&stolenness=stolen"
+      `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=ip&distance=${distance}&stolenness=proximity`
     )
       .then((response) => response.json())
       .then((stolenBikes) => {
